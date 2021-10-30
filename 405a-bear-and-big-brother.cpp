1a1f93302354c5f102e2322daef7413673315db2
j1nxie <rylieeeeexd@gmail.com>
2021-10-30 13:23:39 +0700

    add 405a

---
+++ 405a-bear-and-big-brother.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+		int a, b;
+		cin >> a;
+		cin >> b;
+		int s = 0;
+		
+		while (a <= b) {
+				a = a * 3;
+				b = b * 2;
+				s++;
+		}
+
+		cout << s;
+		return 0;
+}
