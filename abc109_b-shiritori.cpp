09e71d71c4317a8b19bbec182eec84c8f5aedd3e
j1nxie <rylieeeeexd@gmail.com>
2021-10-30 16:46:21 +0700

    add abc109_b (wip)

---
+++ abc109_b-shiritori.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    string w[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> w[i];
+    }
+
+    for (int i = 0; i < n-1; i++) {
+        // wip
+    }
+}
