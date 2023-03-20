7f5ac732901417517ab7cbac8b46423287688ffd
j1nxie <rylieeeeexd@gmail.com>
2023-03-20 09:48:44 +0700

    feat(swe tutorial 01): Person + MobilePhone classes

---
+++ usth/software-engineering/tutorial-01/MobilePhone.java
@@ -0,0 +1,100 @@
+import utils.AttrRef;
+import utils.DOpt;
+import utils.DomainConstraint;
+import utils.NotPossibleException;
+import utils.OptType;
+
+public class MobilePhone {
+	@DomainConstraint(type = "String", mutable = false, optional = false, length = 100)
+	private String manName;
+
+	@DomainConstraint(type = "String", mutable = false, optional = false, length = 50)
+	private String model;
+
+	@DomainConstraint(type = "char", mutable = false, optional = false)
+	private char color;
+
+	@DomainConstraint(type = "int", mutable = false, optional = false)
+	private int year;
+
+	@DomainConstraint(type = "boolean", mutable = false, optional = false)
+	private boolean guaranteed;
+
+	@DOpt(type = OptType.Observer)
+	@AttrRef("manName")
+	public String getManName() {
+		return manName;
+	}
+
+	@DOpt(type = OptType.Observer)
+	@AttrRef("model")
+	public String getModel() {
+		return model;
+	}
+
+	@DOpt(type = OptType.Observer)
+	@AttrRef("color")
+	public char getColor() {
+		return color;
+	}
+
+	@DOpt(type = OptType.Observer)
+	@AttrRef("year")
+	public int getYear() {
+		return year;
+	}
+
+	@DOpt(type = OptType.Observer)
+	@AttrRef("guaranteed")
+	public boolean getGuaranteed() {
+		return guaranteed;
+	}
+
+	public MobilePhone(@AttrRef("manName") String manName, @AttrRef("model") String model, @AttrRef("color") char color,
+			@AttrRef("year") int year, @AttrRef("guaranteed") boolean guaranteed) throws NotPossibleException {
+		if (!validateManName(manName)) {
+			throw new NotPossibleException("invalid manName: " + manName);
+		}
+
+		if (!validateModel(model)) {
+			throw new NotPossibleException("invalid model: " + model);
+		}
+
+		if (!validateColor(color)) {
+			throw new NotPossibleException("invalid color: " + color);
+		}
+
+		this.manName = manName;
+		this.model = model;
+		this.color = color;
+		this.year = year;
+		this.guaranteed = guaranteed;
+	}
+
+	private boolean validateManName(String manName) {
+		if (manName == null || manName.length() == 0 || manName.length() > 100) {
+			return false;
+		}
+
+		return true;
+	}
+
+	private boolean validateModel(String model) {
+		if (model == null || model.length() == 0 || model.length() > 50) {
+			return false;
+		}
+
+		return true;
+	}
+
+	private boolean validateColor(char color) {
+		char[] validcolors = { 'R', 'O', 'Y', 'G', 'B', 'P' };
+		for (char vc : validcolors) {
+			if (color == vc) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
---
+++ usth/software-engineering/tutorial-01/Person.java
@@ -0,0 +1,82 @@
+import utils.AttrRef;
+import utils.DOpt;
+import utils.DomainConstraint;
+import utils.NotPossibleException;
+import utils.OptType;
+
+public class Person {
+	@DomainConstraint(type = "int", mutable = false, optional = false, min = 1)
+	private int id;
+
+	@DomainConstraint(type = "String", mutable = true, optional = false, length = 30)
+	private String name;
+
+	@DomainConstraint(type = "MobilePhone", mutable = true, optional = true)
+	private MobilePhone phone;
+
+	public Person(@AttrRef("id") int id, @AttrRef("name") String name) throws NotPossibleException {
+		if (!validateId(id)) {
+			throw new NotPossibleException("invalid id for person: " + id);
+		}
+
+		if (!validateName(name)) {
+			throw new NotPossibleException("invalid name for person: " + name);
+		}
+
+		this.id = id;
+		this.name = name;
+	}
+
+	@DOpt(type = OptType.Observer)
+	@AttrRef("id")
+	public int getId() {
+		return id;
+	}
+
+	@DOpt(type = OptType.Observer)
+	@AttrRef("name")
+	public String getName() {
+		return name;
+	}
+
+	@DOpt(type = OptType.Observer)
+	@AttrRef("phone")
+	public MobilePhone getPhone() {
+		return phone;
+	}
+
+	@DOpt(type = OptType.Mutator)
+	@AttrRef("name")
+	public void setName(String name) throws NotPossibleException {
+		if (!validateName(name)) {
+			throw new NotPossibleException("invalid name for person: " + name);
+		}
+		this.name = name;
+	}
+
+	@DOpt(type = OptType.Mutator)
+	@AttrRef("phone")
+	public void setPhone(MobilePhone phone) {
+		this.phone = phone;
+	}
+
+	public String greet() {
+		return "Hello " + name + "!";
+	}
+
+	private boolean validateId(int id) {
+		if (id < 1) {
+			return false;
+		}
+
+		return true;
+	}
+
+	private boolean validateName(String name) {
+		if (name == null || name.length() == 0 || name.length() > 30) {
+			return false;
+		}
+
+		return true;
+	}
+}
